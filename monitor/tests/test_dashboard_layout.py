271a9df2fecf02bde4ce8f60fef1f7ad491c3919
Travis Prol <travisjohnprol@gmail.com>
2026-09-08 08:59:14 -0400

    styling

--- monitor/tests/test_dashboard_layout.py
+++ monitor/tests/test_dashboard_layout.py
@@ -64,6 +64,21 @@ def test_dashboard_has_responsive_grid_classes(client, host):
 
 
 @pytest.mark.django_db
+def test_dashboard_stacks_full_width_bands(client, host, host_snapshots, runner):
+    response = client.get("/")
+    content = response.content.decode()
+
+    assert "flex flex-col gap-4" in content
+    assert "xl:col-span-2" not in content
+    assert "xl:col-span-3" not in content
+    assert "xl:grid-cols-4" in content
+    assert 'id="charts-section"' in content
+    assert 'id="runners-section"' in content
+    assert 'id="containers-section"' in content
+    assert "h-72" in content
+
+
+@pytest.mark.django_db
 def test_dashboard_has_kpi_strip_and_icon_chrome(client, host, container, runner):
     response = client.get("/")
     content = response.content.decode()
